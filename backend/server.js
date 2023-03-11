a0642ff7b6904f95c022058d327167b760d8be62
redaman123 <redaghafla@gmail.com>
2023-03-11 23:03:32 +0100

    Delete Backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const { dbConnect } = require("./config/dbConnect");
@@ -14,6 +15,7 @@ dbConnect();
 //Midelware
 
 app.use(express.json());
+app.use(cors({origin: '*'}))
 app.use(express.urlencoded({ extended: true }));  
 
 
