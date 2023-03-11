40a72335492bbd4611fd64a13c4b30d0ffe41e10
redaman123 <redaghafla@gmail.com>
2023-03-11 11:58:27 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const { dbConnect } = require("./config/dbConnect");
+const { router } = require("./routes/todoRoute");
+//PORT
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//DABATABSE
+dbConnect();
+
+//Midelware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));  
+
+
+
+
+
+//ROUTES
+app.use("/api", router);
+// PORT LISTENING
+app.listen(PORT, () =>
+  console.log(
+    `The server is running on Port : ${PORT} -  http://localhost:${PORT}`
+  )
+);
